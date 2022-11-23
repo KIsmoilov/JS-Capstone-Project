5d5fe51d486d528a869a396b7fd3199a7c2ca8c9
Soe Sandar Win <98808636+alyssaackerman2001@users.noreply.github.com>
2022-11-23 20:21:58 +0630

    fix the error of heartbtn function

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,21 +1,45 @@
 import getData from './getdata.js';
 import { displayMealLists  } from './displayMeal.js';
 import { likeGetData, likePostData } from './getLikeData.js';
+ 
 const container = document.getElementById('meal-item-container');
+ 
+const setLikes = (heart, likes) => {
+  const id = heart.closest('article').getAttribute('data-id');
+  const item = likes.find((item) => item.item_id === id);
+  heart.textContent = item.likes;
+}
+ 
+const handleLikeClick = (heart) => {
+  const id = heart.closest('article').getAttribute('data-id');
+  heart.addEventListener('click', async () => {
+    await likePostData(id);
+    const likes = await likeGetData();
+    const hearts = container.querySelectorAll('.like-container');
+    hearts.forEach((heart) => {
+      setLikes(heart, likes);
+    });
+  });
+}
+ 
 const getMealLists = async () => {
   const mealLists = await getData();
-  const displayLikes = await likeGetData();
+  const likes = await likeGetData();
+ 
   mealLists.categories.forEach((meal) => {
-    displayMealLists(meal, displayLikes);
-    const heart = container.lastElementChild.querySelector('.heart-icon');
-    heart.addEventListener('click',async () => {
-      const getId = heart.closest('article').getAttribute('data-id');
-      await likePostData(getId);
-      const displayLikes = await likeGetData();
-      displayMealLists(meal, displayLikes);
-    })
-    
+    displayMealLists(meal); 
   });
+ 
+  const hearts = container.querySelectorAll('.like-container');
+  const heartIcons = container.querySelectorAll('.heart-icon');
+ 
+  hearts.forEach((heart) => {
+    setLikes(heart, likes);
+  });
+ 
+  heartIcons.forEach((heart) => handleLikeClick(heart));
 };
-
+ 
 export default getMealLists;
+
+
--- src/modules/displayMeal.js
+++ src/modules/displayMeal.js
@@ -1,38 +1,31 @@
 import { likeGetData , likePostData } from "./getLikeData.js";
 
 
-const displayMealLists = (meal, likes) => {
+const displayMealLists = (meal) => {
   const container = document.getElementById('meal-item-container');
   const article = document.createElement('article');
-  const itemLike = likes.find((item) => item.item_id === meal.idCategory);
-  article.setAttribute('data-id',meal.idCategory);
+
+  article.setAttribute('data-id', meal.idCategory);
   article.innerHTML = `
   <div class="meal-item">
-                <div class="meal-img">
-                <img src="${meal.strCategoryThumb}" alt="meal images">
-                </div>
-                <div class="meal-name">
-                  <div class="meal-title">
-                  <h3>${meal.strCategory}</h3>
-                   </div>
-                    <div class="like-counter">
-                    <i class="uil uil-heart heart-icon"></i>
-
-                    <p class="like-container">${itemLike.likes}</p>
-                    <p>Likes</p>
-                </div>
-                </div>
-                <button type="button" id="comment-btn">Comment</button>
-            </div>`;
-
-    
-      
-
+    <div class="meal-img">
+      <img src="${meal.strCategoryThumb}" alt="meal images">
+    </div>
+    <div class="meal-name">
+      <div class="meal-title">
+      <h3>${meal.strCategory}</h3>
+        </div>
+        <div class="like-counter">
+        <i class="uil uil-heart heart-icon"></i>
+        <p class="like-container">Loading...</p>
+        <p>Likes</p>
+      </div>
+    </div>
+    <button type="button" id="comment-btn">Comment</button>
+  </div>`;
   container.appendChild(article);
-
-
 };
 
+export { displayMealLists };
 
 
-export {  displayMealLists };
